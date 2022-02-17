e95f8fd0f27783cc9991aa9ded79d70dc650e05e
jesspesale <jess_pesale@aol.com>
2022-02-16 23:33:32 -0500

    added the title list itmes and some styling for it

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -11,7 +11,9 @@ const Intro = () => {
                     <h2 className='i-name'> Jess Pesale</h2>
                     <div className="i-title">
                         <div className="i-title-wrapper">
-                            
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">Software Engineer</div>
+                            <div className="i-title-item">Job Seeker</div>
                         </div>
                     </div>
                 </div>
