f92f160a86f2269ddcde31684b5e82a5678f5c4e
jesspesale <jess_pesale@aol.com>
2022-02-17 01:05:12 -0500

    added purple arrow to left side and put text on top of it - still need to fix scrolling words

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -6,30 +6,32 @@ import Color from "../../image/color-flower.jpeg"
 
 const Intro = () => {
     return (
-        <div className='intro'>
-            <div className='i-left'>
-                <div className="i-left-wrapper">
-                    <h2 className='i-intro'>Hi, my name is</h2>
-                    <h2 className='i-name'> Jess Pesale</h2>
-                    <div className="i-title">
-                        <div className="i-title-wrapper">
-                            <div className="i-title-item">Software Engineer</div>
-                            <div className="i-title-item">Web Developer</div>
-                            <div className="i-title-item">Job Seeker</div>
-                        </div>
-                    </div>
-                    <div className="i-description">
-                        I am a full-stack software developer from New York. With a background in math and business, 
-                        coding is an amazing way to help solve real-wold problems in a creative way.
-                        I love that learning is a never ending process and I can't wait to see what new 
-                        technologies I learn next!
-                    </div>
-                </div>
+      <div className="intro">
+        <div className="i-left">
+          <div className="i-bg"></div>
+          <div className="i-left-wrapper">
+            <h2 className="i-intro">Hi, my name is</h2>
+            <h2 className="i-name"> Jess Pesale</h2>
+            <div className="i-title">
+              <div className="i-title-wrapper">
+                <div className="i-title-item">Software Engineer</div>
+                <div className="i-title-item">Web Developer</div>
+                <div className="i-title-item">Job Seeker</div>
+              </div>
             </div>
-            <div className='i-right'>
-                <img src={Color}/>
+            <div className="i-description">
+              I am a full-stack software developer from New York. With a
+              background in math and business, coding is an amazing way to help
+              solve real-wold problems in a creative way. I love that learning
+              is a never ending process and I can't wait to see what new
+              technologies I learn next!
             </div>
+          </div>
         </div>
+        <div className="i-right">
+          <img src={Color} className="i-image" />
+        </div>
+      </div>
     );
 }
 
