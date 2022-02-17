4087200c118289c0101c7776467ff3f7c1297091
jesspesale <jess_pesale@aol.com>
2022-02-16 22:55:21 -0500

    added intro and name to left side of intro component

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -5,7 +5,17 @@ import "./intro.css"
 const Intro = () => {
     return (
         <div className='intro'>
-            <div className='i-left'>Left</div>
+            <div className='i-left'>
+                <div className="i-left-wrapper">
+                    <h2 className='i-intro'>Hi, my name is</h2>
+                    <h2 className='i-name'> Jess Pesale</h2>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className='i-right'>Right</div>
         </div>
     );
