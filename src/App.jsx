a8bd7cccf0ba5d009001601a2543510366ce9f9e
jesspesale <jess_pesale@aol.com>
2022-02-17 21:04:25 -0500

    fixed navbar settings to stay at the top when you scroll (z-index: 1)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Intro from './components/intro/intro';
+import Navbar from './components/navbar/navbar';
+import About from './components/about/about'
+import Projects from './components/projects/projects'
+import Contact from './components/contact/contact'
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar sticky="top"/>
+      <div className="sections">
+        <Intro/>
+        <About/>
+        <Projects/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
