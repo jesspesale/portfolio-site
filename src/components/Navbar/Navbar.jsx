8fb9b4cd63feeecd89edbdd3a7adb79767142b72
jesspesale <jess_pesale@aol.com>
2022-02-17 19:37:20 -0500

    added navbar basic component and css set up

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+
+const NavBar = () => {
+
+    return(
+        <div className="navbar">
+            <div className="nav-wrapper">
+                <div className="nav-left">
+
+                </div>
+                <div className="nav-right">
+                    
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default NavBar
