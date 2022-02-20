a462cc0186d5ab361dfe18b9c0c947275f5d583e
jesspesale <jess_pesale@aol.com>
2022-02-20 14:42:29 -0500

    added a data page to add all projects in one array

---
+++ src/data/projects.js
@@ -0,0 +1,13 @@
+import mnm from "../images/mnm-crush.png"
+
+export const projects = [
+
+    {
+        name: "MNM Crush",
+        url: "https://mnm-crush.netlify.app/",
+        image: mnm,
+        desc: "M&M crush is a simplified version of the app Candy Crush that I created with just React, HTML and CSS. Move the M&M's to get 3 or 4 in a row, score up to 55 points before 15 turns ad you win!"
+    }
+
+]
+
