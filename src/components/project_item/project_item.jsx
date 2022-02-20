90db7bd4680849a219cc18a3395bf044897a8782
jesspesale <jess_pesale@aol.com>
2022-02-20 16:52:46 -0500

    got two buttons for live project and github repo

--- src/components/project_item/project_item.jsx
+++ src/components/project_item/project_item.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import "./project_item.css"
 
+
 const ProjectItem = ({project}) => {
     {console.log(project)}
     return (
-        <div className='container'>
-            <div className='item'>
-                <h1 className='project-title'>{project.title}</h1>
-                <a href={project.url} target="_blank">
-                    {/* <img src={project.image} className="project-image"/> */}
-                </a>
-                <p className='project-desc'>{project.desc}</p>
+      <div className="container">
+        <div className="item">
+          <a target="_blank">
+            <h1 className="project-title">{project.title}</h1>
+            <p className="project-desc">{project.desc}</p>
+            <div className="buttons">
+              <a className="button" type="submit" href={project.url}>
+                View Project
+              </a>
+              <a className="button" type="submit" href={project.git}>
+                Github Repo
+              </a>
             </div>
+          </a>
         </div>
+      </div>
     );
 }
 
