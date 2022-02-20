115a56e7884800ef44ad95446227e1341c2406e2
jesspesale <jess_pesale@aol.com>
2022-02-20 15:33:50 -0500

    added travel planner as second project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,18 @@
 import mnm from "../images/mnm-crush.png"
+import travelplanner from "../images/travel-planner.png"
 
 export const projects = [
-
-    {
-        id: 1,
-        title: "M&M Crush",
-        url: "https://mnm-crush.netlify.app/",
-        image: mnm,
-        desc: "M&M crush is a simplified version of the app Candy Crush that I created with just React, HTML and CSS. Move the M&M's to get 3 or 4 in a row, score up to 55 points before 15 turns ad you win!"
-    }
-
-]
+  {
+    title: "M&M Crush",
+    url: "https://mnm-crush.netlify.app/",
+    image: mnm,
+    desc: "M&M crush is a simplified version of the app Candy Crush that I created with just React, HTML and CSS. Move the M&M's to get 3 or 4 in a row, score up to 55 points before 15 turns ad you win!",
+  },
+  {
+    title: "Travel Planner",
+    url: "https://travel-plannerr.netlify.app/",
+    img: travelplanner,
+    desc: "Welcome to Travel Planner! This app was created to help you keep track of any upcoming travel plans. I try to travel as much as possible, so organizng all my trip details in one place makes it easier to keep track of! You can add a trip with it's destination and dates and then create a packing list along with a list of things you would like to do while away (itinerary)."
+  },
+];
 
