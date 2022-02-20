642f6854fcd9adbfb7cf872cd042552bfa9c5427
jesspesale <jess_pesale@aol.com>
2022-02-20 16:08:25 -0500

    added some more dumb data for testing visually

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,16 +3,25 @@ import travelplanner from "../images/travel-planner.png"
 
 export const projects = [
   {
+    id: 1,
     title: "M&M Crush",
     url: "https://mnm-crush.netlify.app/",
     image: mnm,
-    desc: "M&M crush is a simplified version of the app Candy Crush that I created with just React, HTML and CSS. Move the M&M's to get 3 or 4 in a row, score up to 55 points before 15 turns ad you win!",
+    desc: "M&M crush is a simplified version of the app Candy Crush. Move the M&M's to get 3 or 4 in a row, score up to 55 points before 15 turns ad you win!",
   },
   {
+    id: 2,
     title: "Travel Planner",
     url: "https://travel-plannerr.netlify.app/",
     img: travelplanner,
-    desc: "Welcome to Travel Planner! This app was created to help you keep track of any upcoming travel plans. I try to travel as much as possible, so organizng all my trip details in one place makes it easier to keep track of! You can add a trip with it's destination and dates and then create a packing list along with a list of things you would like to do while away (itinerary)."
+    desc: "Travel Planner was created to help you keep track of any upcoming travel plans. You can add a trip with a destination and dates and then add a packing list and a list of things you would like to do while away.",
   },
+  {
+    id: 3,
+    title: "Workout Tracker",
+    url: "https://travel-plannerr.netlify.app/",
+    img: travelplanner,
+    desc: "Use workout tracker to keep track of all the calories you burn and what set you do for each workout.",
+  }
 ];
 
