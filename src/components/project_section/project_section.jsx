fcfb0dbfcb072fc85b2753a3adae6770ef0a1316
jesspesale <jess_pesale@aol.com>
2022-02-20 17:45:46 -0500

    got projects centered and inline and wrap to second line when dont fit

--- src/components/project_section/project_section.jsx
+++ src/components/project_section/project_section.jsx
@@ -5,16 +5,18 @@ import ProjectItem from "../project_item/project_item";
 const ProjectSection = () => {
 
   return (
-    <div id="projects">
-      <h2>Projects</h2>
+    <div id="projects" className="projects">
+      <h2 className="proj-sec-title">Here are some of my projects:</h2>
+      <div className="project-items">
       {projects.map((project) => {
         return (
-          <ProjectItem
-            project={project}
-            key={project.id}
-          />
-        );
-      })}
+            <ProjectItem
+              project={project}
+              key={project.id}
+            />
+            );
+          })}
+      </div>
     </div>
   )
 };
