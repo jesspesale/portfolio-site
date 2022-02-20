088b038acf1262770ff691a221555a2520ac3bb8
jesspesale <jess_pesale@aol.com>
2022-02-20 16:19:25 -0500

    added some styling for title and description of projects inside the containers

--- src/components/project_item/project_item.jsx
+++ src/components/project_item/project_item.jsx
@@ -4,13 +4,15 @@ import "./project_item.css"
 const ProjectItem = ({project}) => {
     {console.log(project)}
     return (
-      <div className='item'>
-        <h1 className='project-title'>{project.title}</h1>
-        <a href={project.url} target="_blank">
-            <img src={project.image} className="project-image"/>
-        </a>
-        <p className='project-desc'>{project.desc}</p>
-      </div>
+        <div className='container'>
+            <div className='item'>
+                <h1 className='project-title'>{project.title}</h1>
+                <a href={project.url} target="_blank">
+                    {/* <img src={project.image} className="project-image"/> */}
+                </a>
+                <p className='project-desc'>{project.desc}</p>
+            </div>
+        </div>
     );
 }
 
