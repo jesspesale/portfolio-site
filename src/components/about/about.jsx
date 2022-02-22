4aa5c722ff3690331c90de1feea8a058ccf4d9e2
jesspesale <jess_pesale@aol.com>
2022-02-21 23:01:49 -0500

    working on fixing lots of things - many styling issues

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,17 +2,22 @@ import "./about.css";
 
 const About = () => {
   return (
-    <div className="about" id="about">
-      <div className="a-left">
-        <div className="a-card-bg"></div>
-        <div className="a-card">
-          <p className="a-text">
-            Work out, go for a run, kickboxing, listen to music and podcasts!
-          </p>
-        </div>
+    <div id="about">
+      <div>
+        <h2 className="a-title">About Me</h2>
       </div>
-      <div className="a-right">
-        <div className="a-bg"></div>
+      <div className="about">
+        <div className="a-left">
+          <div className="a-card-bg"></div>
+          <div className="a-card">
+            <p className="a-text">
+              Work out, go for a run, kickboxing, listen to music and podcasts!
+            </p>
+          </div>
+        </div>
+        <div className="a-right">
+          <div className="a-bg"></div>
+        </div>
       </div>
     </div>
   );
