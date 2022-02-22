fb0c8300c23c4b0a6634366873656ffa47844251
jesspesale <jess_pesale@aol.com>
2022-02-21 22:37:14 -0500

    added small box in about section - need to fix

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,9 +4,11 @@ const About = () => {
   return (
     <div className="about" id="about">
       <div className="a-left">
-        <div className="a-card bg"></div>
+        <div className="a-card-bg"></div>
         <div className="a-card">
-
+          <p className="a-text">
+            Work out, go for a run, kickboxing, listen to music and podcasts!
+          </p>
         </div>
       </div>
       <div className="a-right">
