946833fa9ee5f260472e84a04dc9affa6ce3b1a8
jesspesale <jess_pesale@aol.com>
2022-03-28 21:48:47 -0400

    still working on fixing styling for whole app

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,7 +4,6 @@ const Contact = () => {
   return (
     <div className="contact">
         <h2>Contact Information goes here</h2>
-        <h1>Jess</h1>
     </div>
   )
 }
--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -6,41 +6,52 @@ import Color from "../../images/color-flower.png"
 
 const Intro = () => {
     return (
-      <div>
-        <div className="intro" id="intro">
-          <div className="left-color">
-            {/* <div className="i-bg"></div> */}
-            <div className="i-left-wrapper">
-
-              <h2 className="i-intro">Hi, my name is</h2>
-              <h2 className="i-name"> Jess Pesale</h2>
-
-              <div className="i-title">
-                <div className="i-title-wrapper">
-                  <div className="i-title-item">Software Engineer</div>
-                  <div className="i-title-item">Web Developer</div>
-                  <div className="i-title-item">Job Seeker</div>
-                </div>
-              </div>
-
-              <p className="i-description">
-                I am a full-stack software developer from New York. With a
-                background in math and business, coding is an amazing way to
-                help solve real-wold problems in a creative way. I love that
-                learning is a never ending process and I can't wait to see what
-                new technologies I learn next!
-              </p>
-
-            </div>
-
-          </div>
-
-          <div className="i-right">
-            <img src={Color} className="flower-image" alt="colorful flower" />
-          </div>
-        </div>
+      <div className="intro">
+        {/* <br></br><br></br><br></br><br></br><br></br> */}
+        <h1>Intro</h1>
       </div>
     );
 }
 
 export default Intro;
+
+// const Intro = () => {
+//     return (
+//       <div>
+//         <div className="intro" id="intro">
+//           <div className="left-color">
+//             {/* <div className="i-bg"></div> */}
+//             <div className="i-left-wrapper">
+
+//               <h2 className="i-intro">Hi, my name is</h2>
+//               <h2 className="i-name"> Jess Pesale</h2>
+
+//               <div className="i-title">
+//                 <div className="i-title-wrapper">
+//                   <div className="i-title-item">Software Engineer</div>
+//                   <div className="i-title-item">Web Developer</div>
+//                   <div className="i-title-item">Job Seeker</div>
+//                 </div>
+//               </div>
+
+//               <p className="i-description">
+//                 I am a full-stack software developer from New York. With a
+//                 background in math and business, coding is an amazing way to
+//                 help solve real-wold problems in a creative way. I love that
+//                 learning is a never ending process and I can't wait to see what
+//                 new technologies I learn next!
+//               </p>
+
+//             </div>
+
+//           </div>
+
+//           <div className="i-right">
+//             <img src={Color} className="flower-image" alt="colorful flower" />
+//           </div>
+//         </div>
+//       </div>
+//     );
+// }
+
+// export default Intro;
