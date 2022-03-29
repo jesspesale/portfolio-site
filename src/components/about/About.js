a63250dc05ca74d9c23f0001bdb9784869de076a
jesspesale <jess_pesale@aol.com>
2022-03-28 21:26:05 -0400

    deleted some things from about section, keeping it basic till get the styling down

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,19 +6,18 @@ const About = () => {
       <div>
         <h2 className="a-title">About Me</h2>
       </div>
-      {/* <div className="about">
-        <div className="a-left">
-          <div className="a-card-bg"></div>
-          <div className="a-card"> */}
-            <p className="a-text">
-              Work out, go for a run, kickboxing, listen to music and podcasts!
-            </p>
-          {/* </div>
-        </div>
+      <div className="about">
+        {/* <div className="a-left"> */}
+            <div className="a-card">
+              <p className="a-text">
+                Work out, go for a run, kickboxing, listen to music and podcasts!
+              </p>
+            </div>
+        {/* </div> */}
         <div className="a-right">
           <div className="a-bg"></div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
