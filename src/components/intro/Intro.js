1b4cc083414007c6238e68992338a38957619e20
jesspesale <jess_pesale@aol.com>
2022-03-29 22:21:35 -0400

    removed some useless divs and added css to put all words on the left side of the page

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -7,8 +7,37 @@ import Color from "../../images/color-flower.png"
 const Intro = () => {
     return (
       <div className="intro">
-        {/* <br></br><br></br><br></br><br></br><br></br> */}
-        <h1>Intro</h1>
+        <div >
+          {/* <div className="left-color"> */}
+            <div className="i-left">
+
+              <h2 className="i-intro">Hi, my name is</h2>
+              <h2 className="i-name"> Jess Pesale</h2>
+
+              <div className="i-title">
+                <div className="i-title-wrapper">
+                  <div className="i-title-item">Software Engineer</div>
+                  <div className="i-title-item">Web Developer</div>
+                  <div className="i-title-item">Job Seeker</div>
+                </div>
+              </div>
+
+              <p className="i-description">
+                I am a full-stack software developer from New York. With a
+                background in math and business, coding is an amazing way to
+                help solve real-wold problems in a creative way. I love that
+                learning is a never ending process and I can't wait to see what
+                new technologies I learn next!
+              </p>
+
+            </div>
+
+          {/* </div> */}
+
+          <div className="i-right">
+            <img src={Color} className="flower-image" alt="colorful flower" />
+          </div>
+        </div>
       </div>
     );
 }
